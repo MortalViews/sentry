3467638bb1f87ba8aa29ee0ce5cf7e758aca6271
Ted Kaemming <ted@kaemming.com>
2017-08-07 18:02:35 -0700

    almost working

--- src/sentry/scripts/digests/digests.lua
+++ src/sentry/scripts/digests/digests.lua
@@ -37,120 +37,136 @@ local function maintenance(deadline)
     error('not implemented')
 end
 
-local function add_timeline_to_schedule(timeline, timestamp)
+local function add_timeline_to_schedule(configuration, timeline_id, timestamp, increment, maximum)
     -- If the timeline is already in the "ready" set, this is a noop.
-    if redis.call('ZSCORE', ready, timeline) ~= nil then
+    if redis.call('ZSCORE', configuration:get_schedule_ready_key(), timeline_id) ~= nil then
         return false
     end
 
     -- Do scheduling if the timeline is not already in the "ready" set.
-    local score = redis.call('ZSCORE', waiting, timeline)
+    local score = redis.call('ZSCORE', configuration:get_schedule_waiting_key(), timeline_id)
     if score ~= nil then
         -- If the timeline is already in the "waiting" set, increase the delay by
         -- min(current schedule + increment value, maximum delay after last processing time).
-        local last_processed = tonumber(redis.call('GET', last_processed_timestamp))
+        local last_processed = tonumber(redis.call('GET', configuration:get_timeline_last_processed_timestamp_key(timeline_id)))
         local update = nil;
         if last_processed == nil then
-            -- If the last processed timestamp is missing for some reason (possibly
-            -- evicted), be conservative and allow the timeline to be scheduled
-            -- with either the current schedule time or provided timestamp,
-            -- whichever is smaller.
+            -- If the last processed timestamp is missing for some reason
+            -- (possibly evicted), be conservative and allow the timeline to be
+            -- scheduled with either the current schedule time or provided
+            -- timestamp, whichever is smaller.
             update = math.min(score, timestamp)
         else
             update = math.min(
-                score + tonumber(increment),
-                last + tonumber(maximum)
+                score + increment,
+                last_processed + maximum
             )
         end
 
         if update ~= score then
-            redis.call('ZADD', waiting, update, timeline)
+            -- This should technically be ZADD XX for correctness (this item
+            -- should always exist, and we established that above) but not
+            -- using that here doesn't break anything and allows use to use
+            -- older Redis versions.
+            redis.call('ZADD', configuration:get_schedule_waiting_key(), update, timeline_id)
         end
+
         return false
     end
 
     -- If the timeline isn't already in either set, add it to the "ready" set with
     -- the provided timestamp. This allows for immediate scheduling, bypassing the
-    -- imposed delay of the "waiting" state.
-    redis.call('ZADD', READY, TIMESTAMP, TIMELINE)
+    -- imposed delay of the "waiting" state. (This should also be ZADD NX, but
+    -- like above, this allows us to still work with older Redis.)
+    redis.call('ZADD', configuration:get_schedule_ready_key(), timestamp, timeline_id)
+
     return true
 end
 
-local function truncate_timeline(timeline, limit)
+local function truncate_timeline(configuration, timeline_id, timeline_capacity)
     local n = 0
 
     -- ZCARD is O(1) while ZREVRANGE is O(log(N)+M) so as long as digests are
     -- generally smaller than the limit (which seems like a safe assumption)
     -- then its cheaper just to check here and exit if there's nothing to do.
-    if redis.call('ZCARD', timeline) <= limit then
+    local timeline_key = configuration:get_timeline_key(timeline)
+    if redis.call('ZCARD', timeline_key) <= limit then
         return n
     end
 
-    local records = redis.call('ZREVRANGE', timeline, limit, -1)
-    for _, key in ipairs(records) do
-        redis.call('ZREM', key)
-        redis.call('DEL', key)
+    local records = redis.call('ZREVRANGE', timeline_key, timeline_capacity, -1)
+    for _, record_id in ipairs(records) do
+        redis.call('ZREM', timeline_key, record_id)
+        redis.call('DEL', configuration:get_timeline_record_key(timeline_id, record_id))
         n = n + 1
     end
     return n
 end
 
-local function add_record_to_timeline(timeline, key, value, timestamp)
-    -- Store the record in the database.
-    redis.call('SETEX', key, value, configuration.ttl)
-    redis.call('EXPIRE', timeline, configuration.ttl)
+local function add_record_to_timeline(configuration, timeline_id, record_id, value, timestamp, delay_increment, delay_maximum, timeline_capacity, truncation_chance)
+    redis.call('SETEX', configuration:get_timeline_record_key(timeline_id, record_id), configuration.ttl, value)
+    redis.call('ZADD', configuration:get_timeline_key(timeline_id), timestamp, record_id)
+    redis.call('EXPIRE', configuration:get_timeline_key(timeline_id), configuration.ttl)
 
-    redis.call('ZADD', timeline, timetamp, key)
-
-    local ready = add_timeline_to_schedule(timeline, timestamp)
+    local ready = add_timeline_to_schedule(configuration, timeline_id, timestamp, delay_increment, delay_maximum)
 
     if math.random() < truncation_chance then
-        truncate_timeline(timeline, capacity)
+        truncate_timeline(configuration, timeline_id, timeline_capacity)
     end
 
     return ready
 end
 
-local function digest_timeline(timeline)
+local function digest_timeline(configuration, timeline_id)
     -- Check to ensure that the timeline is in the correct state.
+    if redis.call('ZSCORE', configuration:get_schedule_ready_key(), timeline_id) == nil then
+        error('timeline is not in the ready state, cannot be digested')
+    end
 
-    if redis.call('EXISTS', digest) == 1 then
+    local digest_key = configuration:get_timeline_digest_key(timeline_id)
+    local timeline_key = configuration:get_timeline_key(timeline_id)
+    if redis.call('EXISTS', digest_key) == 1 then
         -- If the digest set already exists (possibly because we already tried
         -- to send it and failed for some reason), merge any new data into it.
-        redis.call('ZUNIONSTORE', digest, 2, timeline, digest, 'AGGREGATE', 'MAX')
-        redis.call('DELETE', timeline)
-        redis.call('EXPIRE', digest, configuration.ttl)
+        redis.call('ZUNIONSTORE', digest_key, 2, timeline_key, digest_key, 'AGGREGATE', 'MAX')
+        redis.call('DELETE', timeline_key)
+        redis.call('EXPIRE', digest_key, configuration.ttl)
     else
         -- Otherwise, we can just move the timeline contents to the digest key.
-        redis.call('RENAME', timeline, digest)
-        redis.call('EXPIRE', digest, configuration.ttl)
+        redis.call('RENAME', timeline_key, digest_key)
+        redis.call('EXPIRE', digest_key, configuration.ttl)
     end
 
-    local records = redis.call('ZREVRANGE', digest, 0, -1, 'WITHSCORES')
+    local results = {}
+    local records = redis.call('ZREVRANGE', digest_key, 0, -1, 'WITHSCORES')
     for key, score in zrange_scored_iterator(records) do
-        error('return records')
+        table.insert(results, {key, score})
     end
 
-    -- Return the records to the client.
+    return results
 end
 
-local function close_digest(timeline, records)
-    -- Remove the records from the digest.
-    redis.call('ZREM', digest, unpack(records))
-    for _, record in ipairs(records) do
-        redis.call('DEL', record)
+local function close_digest(configuration, timeline_id, record_ids, delay_minimum)
+    local timeline_key = configuration:get_timeline_key(timeline_id)
+    local digest_key = configuration:get_timeline_digest_key(timeline_id)
+
+    redis.call('ZREM', digest_key, unpack(record_ids))
+
+    for _, record_id in ipairs(record_ids) do
+        -- TODO: This could technically be called as a variadic instead, if it mattered.
+        redis.call('DEL', configuration:get_timeline_record_key(timeline_id, record_id))
     end
 
     -- We always add to the ready set if we digested any number of records or
     -- there are contents waiting to be delivered.
-    if #records > 0 or redis.call('ZCARD', timeline) > 0 or redis.call('ZCARD', digest) > 0 then
-        redis.call('SETEX', last_processed, configuration.ttl, value)
-        redis.call('ZREM', ready, timeline)
-        redis.call('ZADD', waiting, timestamp + minimum_delay, timeline)
+    if #record_ids > 0 or redis.call('ZCARD', timeline_key) > 0 or redis.call('ZCARD', digest_key) > 0 then
+        redis.call('SETEX', configuration:get_timeline_last_processed_timestamp_key(timeline_id), configuration.ttl, configuration.timestamp)
+        redis.call('ZREM', configuration:get_schedule_ready_key(), timeline_id)
+        redis.call('ZADD', configuration:get_schedule_waiting_key(), configuration.timestamp + delay_minimum, timeline_id)
     else
-        redis.call('DEL', last_processed)
-        redis.call('ZREM', ready, timeline)
-        redis.call('ZREM', waiting, timeline)
+        redis.call('DEL', configuration:get_timeline_last_processed_timestamp_key(timeline_id))
+        redis.call('ZREM', configuration:get_schedule_ready_key(), timeline_id)
+        redis.call('ZREM', configuration:get_schedule_waiting_key(), timeline_id)
     end
 end
 
@@ -163,11 +179,13 @@ local function delete_timeline(configuration, timeline_id)
 end
 
 local function parse_arguments(arguments)
-    -- TODO: fill in
     local configuration = {
         namespace = 'd',
         ttl = 60 * 60,
+        timestamp = 0,   -- TODO: fill in
     }
+
+    math.randomseed(configuration.timestamp)
 
     function configuration:get_schedule_waiting_key()
         return string.format('%s:s:w', self.namespace)
@@ -207,18 +225,18 @@ local commands = {
     end
     ADD = function (keys, arguments)
         local configuration, arguments = parse_arguments(arguments)
-        error('not implemented')
+        return add_record_to_timeline(configuration, unpack(arguments))
     end,
     DELETE = function (keys, arguments)
         local configuration, arguments = parse_arguments(arguments)
-        error('not implemented')
+        return delete_timeline(configuration, unpack(arguments))
     end,
     DIGEST_OPEN = function (keys, arguments)
         local configuration, arguments = parse_arguments(arguments)
-        error('not implemented')
+        return digest_timeline(configuration, unpack(arguments))
     end,
     DIGEST_CLOSE = function (keys, arguments)
         local configuration, arguments = parse_arguments(arguments)
-        error('not implemented')
+        return close_digest(configuration, unpack(arguments))
     end,
 }
